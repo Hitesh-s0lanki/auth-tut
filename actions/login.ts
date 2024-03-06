ddc734ed7fb3803adca360f79a5a2d605739beda
Hitesh Solanki <hiteshsolanki4623@gmail.com>
2024-03-06 20:06:58 +0530

    prisma updae

--- actions/login.ts
+++ actions/login.ts
@@ -12,7 +12,7 @@ import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 import * as z from "zod";
 
-export async function login(values: z.infer<typeof LoginSchema>, callbackUrl?: string) {
+export async function login(values: z.infer<typeof LoginSchema>, callbackUrl?: string | null) {
     try {
         const validatedFields = LoginSchema.safeParse(values)
 
